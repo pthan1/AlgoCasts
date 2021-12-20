17a308c970fb76c9bca25f6231a3b8ffc557b88a
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-12-19 18:09:52 -0700

    Find number of vowels in a string

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,43 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+//My solution
+// pseudocode
+//have a counter start at 0
+//iterate through String
+//if character is a e i or u , increment counter
+// return counter
+
+function vowels(str) {
+  let counter = 0;
+  const lowercase = str.toLowerCase();
+  for (let char of lowercase) {
+    if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+      counter++;
+    }
+  }
+
+  return counter;
+}
+
+
+// alternate solution 1
+// function vowels(str) {
+//   let counter = 0;
+//   const checker = 'aeiou' // or checker = ['a', 'e', 'i', 'o', 'u']
+//   for (let char of str.toLowerCase()) {
+//     if (checker.includes(char)) {
+//       counter++;
+//     }
+//   }
+
+//   return counter;
+// }
+
+// alternative solution 2: regex solution
+// function vowels(str) {
+//   const matches = str.match(/[aeiou]/gi);
+//   return matches ? matches.length : 0;
+// }
 
 module.exports = vowels;
